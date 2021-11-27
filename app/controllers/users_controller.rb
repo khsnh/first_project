70f32cbace1d3b1d870594857691c77ca01cdb65
Khsnh <ngx.k2210001@gmail.com>
2021-11-27 19:42:17 +0700

    following-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
-  before_action :find_user, only: [:show, :edit, :destroy]   
-  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :find_user, only: [:show, :edit, :destroy, :followers , :following]   
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy, :following, :followers]
   before_action :correct_user,only: [:edit, :update]
   before_action :admin_user,only: :destroy
 
@@ -48,6 +48,19 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted"
     redirect_to users_url
   end
+
+  def following
+    @title = "Following"
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
 
   private
   def user_params
